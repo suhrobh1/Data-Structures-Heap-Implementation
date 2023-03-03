46fb942881c5b3bc69d5dfe30f85eadb329d659b
suhrobh1 <93023422+suhrobh1@users.noreply.github.com>
2023-03-03 10:20:04 -0800

    Update min_heap.py

--- min_heap.py
+++ min_heap.py
@@ -177,10 +177,13 @@ def _percolate_down(da: DynamicArray, node_to_check_index: int) -> None:
             smaller_child_index = child_2_index
 
         if node_to_check > smaller_child:
-            temp = da[node_to_check_index]
-            da[node_to_check_index] = smaller_child
-            da[smaller_child_index] = temp
+            # temp = da[node_to_check_index]
+            # da[node_to_check_index] = smaller_child
+            # da[smaller_child_index] = temp
 
+
+            da[node_to_check_index], da[smaller_child_index] = da[smaller_child_index], da[node_to_check_index]
+           
             node_to_check_index = smaller_child_index
             node_to_check = da[node_to_check_index]
             child_1_index = 2 * node_to_check_index + 1
@@ -195,19 +198,15 @@ def _percolate_down(da: DynamicArray, node_to_check_index: int) -> None:
             #         da[child_1_index] = node_to_check
 
         # Outside of while loop
-        # if child_1_index == length - 1:
-        #     # print()
-        #     if node_to_check > da[child_1_index]:
-        #         print("Woof")
-        #         da[node_to_check_index] = da[child_1_index]
-        #         da[child_1_index] = node_to_check
+        if child_1_index == length - 1:
+            # print()
+            if node_to_check > da[child_1_index]:
+                print("Woof")
+                da[node_to_check_index] = da[child_1_index]
+                da[child_1_index] = node_to_check
 
         
-        # print()
-        if node_to_check > da[child_1_index]:
-            print("Woof")
-            da[node_to_check_index] = da[child_1_index]
-            da[child_1_index] = node_to_check
+      
 
 # def _percolate_down(da: DynamicArray, parent: int) -> None:
 #     """
