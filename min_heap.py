6808b901c9e4373f1109244d87a1d026da176f38
Suhrob Hasanov <suhrobhasanov1@gmail.com>
2023-03-02 23:23:49 -0800

    2

--- min_heap.py
+++ min_heap.py
@@ -99,9 +99,10 @@ class MinHeap:
 
         return_value = self._heap[0]
 
-        if self._heap.length() == 0:
+        if self._heap.is_empty():
             raise MinHeapException
         elif self._heap.length() == 1:
+            self._heap.remove_at_index(0)
             return return_value
        
 
